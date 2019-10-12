9b140e61755cc556d11fa1c3a4ee4f486a04bc7f
kenyv <kenyv@DESKTOP-9ONC57S>
2019-10-12 03:05:48 -0600

    Ventana principal del ERP para el proyecto final. Actualmente posee completa la parte grafica del codigo.

---
+++ Inventarios1/AlmacenUMG/src/almacenumg/AlmacenUMG.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package almacenumg;
+
+/**
+ *
+ * @author kenyv
+ */
+public class AlmacenUMG {
+
+    public static void main(String[] args) {
+        Principal p1 = new Principal();
+        p1.setVisible(true);
+        p1.setLocationRelativeTo(null);
+    }
+    
+}
